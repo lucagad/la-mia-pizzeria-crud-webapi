3a4184156a6826ead371f7b609b2cd39698d2109
lucagaddini <lucagaddini@icloud.com>
2022-10-14 18:02:14 +0200

    API GET Pizza

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using la_mia_pizzeria_post.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace la_mia_pizzeria_crud_webapi.Controllers.API
+{
+    [Route("Guest/api/[controller]")]
+    //[Route("[Page]/api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        PizzaContext db;
+
+        public PizzaController()
+        {
+            db = new PizzaContext();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> pizzes = db.Pizzas.ToList();
+
+            return Ok(pizzes);
+        }
+    }
+}
---
+++ Controllers/GuestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_post.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_crud_webapi.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PizzasList()
+        {
+            return View();
+        }
+
+    }
+}
